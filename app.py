4f75b7e5e3eab217e7ee54779535add91ea49b05
rsanchez <rsanchez@cenace.org.ec>
2018-04-16 16:13:23 -0500

    Implementing Ecuador maps

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import json
 
 # default code
 app = Flask(__name__)
@@ -10,7 +11,20 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route("/test/<string:name>/")
+def test(name):
+    """ This is a testing path """
+    return render_template('test.html', name=name)
+
+
 """ Include new pages below this """
 
+
+@app.route("/map")
+def create_map():
+    map_data = json.load(open('./App_data/maps/ecuador_xy.json'))
+    return render_template('Ecuador_Map.html', map_data=map_data)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=80)
